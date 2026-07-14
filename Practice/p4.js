f26474c1fddba6b0778b4f7468a1e95c6759d129
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-15 00:27:57 +0530

    solve problem on javascript

---
+++ Practice/p4.js
@@ -0,0 +1,7 @@
+// JavaScript Arrays
+
+const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THIS LINE!
+
+//Correcting the string 'Lua' to 'Luna' and 'Bellatrix' to 'Draco'
+leaderboard[1] = "Luna";
+leaderboard[3] = "Draco";
