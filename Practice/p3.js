d465fd0a0df883516f596d15ca2a610dc4c429a1
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-15 00:43:00 +0530

    change code line

--- Practice/p3.js
+++ Practice/p3.js
@@ -1,6 +1,6 @@
 const mystery = "Pavanbi7"; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
 
 // LEAVE THIS CODE ALONE! (pretty please)
-if (mystery[0] === "P" && mystery.length > 5 && mystery.indexOf("7") !== -1) {
+if (mystery.startsWith("P") && mystery.length > 5 && mystery.includes("7")) {
   console.log("YOU GOT IT!!!");
 }
--- Practice/p4.js
+++ Practice/p4.js
@@ -1,7 +1,14 @@
 // JavaScript Arrays
 
-const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THIS LINE!
+const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"];
 
-//Correcting the string 'Lua' to 'Luna' and 'Bellatrix' to 'Draco'
-leaderboard[1] = "Luna";
-leaderboard[3] = "Draco";
+const nameCorrections = {
+  Lua: "Luna",
+  Bellatrix: "Draco",
+};
+
+leaderboard.forEach((name, index) => {
+  if (nameCorrections[name]) {
+    leaderboard[index] = nameCorrections[name];
+  }
+});
