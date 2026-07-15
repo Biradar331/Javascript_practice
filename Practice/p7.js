edf40915fd582ac83858282e4e14f309db72f2fa
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-16 00:08:51 +0530

    solve problem on javascript objects

---
+++ Practice/p7.js
@@ -0,0 +1,15 @@
+// Javascript Objects and Arrays Practice
+
+// code goes in here:
+
+const product = {
+  name: "Gummy Bears",
+  inStock: true,
+  price: 1.99,
+  flavors: ["grape", "apple", "cherry"],
+};
+
+console.log(product.name); // prints "Gummy Bears"
+console.log(product.inStock); // prints true
+console.log(product.price); // prints 1.99
+console.log(product.flavors); // prints ["grape", "apple", "cherry"]
