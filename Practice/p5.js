191ed5fff382971660bdccf834d3eba32de5d56b
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-15 22:27:35 +0530

    solve problem on Javascript arrays

---
+++ Practice/p5.js
@@ -0,0 +1,12 @@
+// Javascript problem on Arrays.
+
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"];
+
+planets.shift("The Moon");
+console.log(planets);
+
+planets.push("Saturn");
+console.log(planets);
+
+planets.unshift("Mercury");
+console.log(planets);
